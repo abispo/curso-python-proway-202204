f44c5123e72679f686857acdc125d459ea6116b7
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-05-09 22:11:47 -0300

    Arquivos aula 20220509

--- modulo_03_web/modulo_03_web/20220509_aula03/polls/views.py
+++ modulo_03_web/modulo_03_web/20220509_aula03/polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 # Function based views
@@ -44,4 +44,20 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Você está votando na pergunta {question_id}.")
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question': question,
+            'error_message': 'Você deve escolher uma opção!'
+        }
+        return render(request, 'polls/detail.html', context=context)
+
+    selected_choice.votes += 1
+    selected_choice.save()
+
+    # /polls/4/results
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
